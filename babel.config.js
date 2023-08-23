cea6fa52a14d7875bbc139db77990f04c67e153e
sofienne.lassoued <sofienne.lassoued@hidopi.com>
2023-08-23 19:56:41 +0100

    Fixed app exiting

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: [path.resolve('./src')],
+        extensions: ['.js', '.jsx', '.json', '.svg', '.png'],
+        alias: {
+          '@navigators': './src/navigators',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@state': './src/state',
+          '@domain': './src/domain',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@styles': './src/styles',
+          '@theme': './src/theme',
+          '@assets': './assets',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/components/notifications-item/index.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  card: {},
+  containerHeader: {
+    padding: 8,
+    flexDirection: 'row',
+  },
+  containerHeaderTitles: {
+    flex: 1,
+  },
+  textSubtitle:{
+    fontSize: 12,
+    fontWeight: 400
+  },
+  containerHeaderIcon: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 48,
+  },
+  containerHeaderIconHolder: {
+    padding: 6,
+    borderRadius: 24,
+  },
+});
+export default styles;
