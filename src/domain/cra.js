23332d6deb280c5ce3e7c3b2b40c167d8d97b112
sofienne.lassoued <sofienne.lassoued@hidopi.com>
2023-09-04 01:35:32 +0100

    Added pagination in CRA History

--- src/domain/cra.js
+++ src/domain/cra.js
@@ -11,14 +11,31 @@ const postCRA = async payload => {
   });
 };
 
-const getCRAHistory = async () => {
-  const { uid } = await getAuthorization();
-  return axios.get(`${ENDPOINT}/consultant/historique-cra/${uid}`);
+const getCRAHistory = async ({ sort = 'DESC', page = 1, limit = 2 } = {}) => {
+  // const { uid } = await getAuthorization();
+  const authorization = await getAuthorization();
+  const uid = '64820b713937a729af5cc814';
+  return axios.get(
+    `${ENDPOINT}/consultant/historique-cra/${uid}?sort=${sort}&limit=${limit}&page=${page}`,
+    {
+      headers: {
+        authorization,
+      },
+    },
+  );
 };
 
-const getCRAHistory2 = async () => {
-  const { uid } = await getAuthorization();
-  return axios.get(`${ENDPOINT}/cra/getall-cra`);
+const getAllCRAs = async ({ sort = 'DESC', page = 0, limit = 1 } = {}) => {
+  // const { uid } = await getAuthorization();
+  const authorization = await getAuthorization();
+  return axios.get(
+    `${ENDPOINT}/cra/getall-cra?sort=${sort}&limit=${limit}&page=${page}`,
+    {
+      headers: {
+        authorization,
+      },
+    },
+  );
 };
-const getCRAHistory_ = getCRAHistory2;
-export { getCRAHistory, getCRAHistory_, postCRA };
+
+export { getCRAHistory, getAllCRAs, postCRA };
