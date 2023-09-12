63cc0bffe1b5f44000ba92781f4893e43b7e0e31
sofienne.lassoued <sofienne.lassoued@hidopi.com>
2023-09-12 12:53:13 +0100

    Fixed translations exporting

--- src/utils/translations/index.js
+++ src/utils/translations/index.js
@@ -1,12 +1,10 @@
 import { I18n } from 'i18n-js';
 import en from './en.json';
 import fr from './fr.json';
-import it from './it.json';
 
 const i18n = new I18n({
   en,
   fr,
-  it,
 });
-const t = i18n.t;
-export { i18n, t };
+
+export { i18n };
