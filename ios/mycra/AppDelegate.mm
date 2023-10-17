7ce6f65246d754cad84f2616659de4283ce1d3d9
hidopi-tunisia <h.chebbi@hidopi.com>
2023-10-17 20:41:07 +0200

    ios Worked + fix bugs translate

--- ios/mycra/AppDelegate.mm
+++ ios/mycra/AppDelegate.mm
@@ -1,17 +1,20 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import "RNSplashScreen.h"
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"mycra";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  [RNSplashScreen show];
+  [RNSplashScreen hide];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
